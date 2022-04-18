3667736ca685646f5e187c46d1d022b83403f8d4
HwijooJang <jhj9469@naver.com>
2022-04-18 19:41:08 +0900

    인프런 김영한님 스프링 강의 11강까지

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        Member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // Null값을 감싸기 위해 Optional.ofNullable 을 사용한다
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny(); // 람다식 로직
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // 실무에서는 List를 많이 쓴다.
+    }
+}
