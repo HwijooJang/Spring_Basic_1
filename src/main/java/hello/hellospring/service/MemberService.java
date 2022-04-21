f684a272f6ed32ea1e6e6985d0915712a9a7f836
HwijooJang <jhj9469@naver.com>
2022-04-21 15:26:04 +0900

    13강 수강.

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -30,7 +30,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) { // 같은 이름이 있는 중복 회원X
         memberRepository.findByName(member.getName()).ifPresent(m -> { // 컨트롤 + 알트 + v 으로 하면 옵셔널이 생성된다.
             throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }); // 람다식과 IllegalStateException을 사용해서 중복하는 회원을 걸러낸다. (null이 아니라 값이 있으면 동작한다.)
+        }); // 람다식과 IllegalStateException을 사용해서 중복하는 회원을 걸러낸다. (null이 아니라 값이 있으면 동작한다)
     }
 
     // 전체 회원 조회
