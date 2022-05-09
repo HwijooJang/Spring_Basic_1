0af08c18afd778929e80e099f548fe0a4e3dcace
HwijooJang <jhj9469@naver.com>
2022-05-09 21:02:22 +0900

    회원 웹 기능 홈 화면 조회기능

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController { // @Controller를 적는 순간 스프링 컨테이너가 생기는데 거기에 MemberController를 생성하고 관리를 한다
@@ -43,6 +46,14 @@ public class MemberController { // @Controller를 적는 순간 스프링 컨테
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
     }
 }
 
